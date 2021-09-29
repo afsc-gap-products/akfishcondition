0f65c6a0fe85a0720c47835a0566daa3dc6ee642
sean-rohan-NOAA <sean.rohan@noaa.gov>
2021-09-29 12:40:45 -0700

    Include corner stations

--- inst/sql/ebs_length_weight.sql
+++ inst/sql/ebs_length_weight.sql
@@ -13,7 +13,7 @@ decode(a.stratum, 31, 30, 32, 30, 61, 60, 62, 60, 41, 40, 42, 40, 43, 40, a.stra
 a.stationid, a.bottom_depth, a.start_latitude, a.start_longitude, d.specimenid,
 e.common_name, d.sex, d.length, d.weight, b.weight catch_weight
 from racebase.haul a, racebase.catch b, racebase.specimen d , racebase.species e, race_data.v_cruises c
-where a.region = 'BS' and a.cruise >= 199900 and a.stationid like '%-%'
+where a.region = 'BS' and a.cruise >= 199900
 and b.species_code in (21740, 21741, 21720, 10110, 10210, 10130, 10261, 10285)
 and a.hauljoin = b.hauljoin and b.hauljoin = d.hauljoin
 and b.species_code = e.species_code and b.species_code = d.species_code and d.length != 0 and d.weight != 0
