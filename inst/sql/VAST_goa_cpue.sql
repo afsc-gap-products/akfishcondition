8c4c1c957db2bf78d38458881bd4d571af710c08
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-05-02 09:37:24 -0700

    Add numerical catch to CPUE queries

--- inst/sql/VAST_goa_cpue.sql
+++ inst/sql/VAST_goa_cpue.sql
@@ -1,7 +1,7 @@
 /* Query to retrieve CPUE in kilograms per square kilometer from all Gulf of Alaska strata for VAST. */
 
 select a.hauljoin, a.year, c.start_latitude latitude, c.start_longitude longitude, c.start_time,
-	b.species_code, common_name, a.wgtcpue cpue_kg_km2, a.number_fish, a.effort
+	b.species_code, common_name, a.wgtcpue cpue_kg_km2, a.number_fish, a.effort effort_km2
 	from goa.cpue a, racebase.species b, racebase.haul c, race_data.cruises f, race_data.surveys g
 	where a.species_code = b.species_code
 	and a.hauljoin = c.hauljoin
