ee295aaac3b948eb429b063f45398613c6286201
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-09-16 17:37:38 -0700

    Very preliminary data update

--- analysis/ex_run_vast_with_akfishcondition.R
+++ analysis/ex_run_vast_with_akfishcondition.R
@@ -2,6 +2,8 @@ library(akfishcondition)
 
 akfishcondition::get_condition_data(channel = akfishcondition:::get_connected(schema = "AFSC"))
 
-region <- "GOA"
-x <- dplyr::filter(akfishcondition::ESR_SETTINGS$VAST_SETTINGS, region == region)
-akfishcondition::run_vast_condition(x = x, region = region, n_knots = 400, response = "count")
+
+x <-  dplyr::filter(akfishcondition::ESR_SETTINGS$VAST_SETTINGS, 
+                    region == "AI",
+                    species_code == 30060)
+akfishcondition::run_vast_condition(x = x, region = "AI", n_knots = 600, response = "count")
